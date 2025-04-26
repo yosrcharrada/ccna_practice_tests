6dd56e77ff6b321e5d9be7849ded29f3ee300cfd
charradamohamed <mohamed.charrrada@gmail.com>
2025-04-26 09:39:37 +0100

    W6 Adding React state for the searchTerm

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
-import * as React from "react";
-function getTitle(title) {
-  return title;
-}
-function App() {
+import React from 'react';
+
+const stories = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
+const App = () => {
   return (
     <div>
-      <h1>Hello {getTitle("React")}</h1>
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+      <h1>My Stories</h1>
+      <ul>
+        {stories.map(item => (
+          <li key={item.objectID}>
+            {item.title} by {item.author}
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
-export default App;
+};
