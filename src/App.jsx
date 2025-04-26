74386e6ba5805b9b74cb5fc8d4d27b865ad90f27
charradamohamed <mohamed.charrrada@gmail.com>
2025-04-26 11:25:26 +0100

    code

--- src/App.jsx
+++ src/App.jsx
@@ -20,10 +20,14 @@ const App = () => {
     },
   ];
 
+  const handleSearch = (event) => {
+    console.log(event.target.value);
+  };
+
   return (
     <div>
       <h1>My Hacker Stories</h1>
-      <Search />
+      <Search onSearch={handleSearch} />
       <hr />
       <List list={stories} />
     </div>
@@ -49,20 +53,18 @@ const Item = (props) => (
   </li>
 );
 
-const Search = () => {
+const Search = (props) => {
   const [searchTerm, setSearchTerm] = React.useState('');
 
   const handleChange = (event) => {
     setSearchTerm(event.target.value);
+    props.onSearch(event);
   };
 
   return (
     <div>
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" onChange={handleChange} />
-      <p>
-        Searching for <strong>{searchTerm}</strong>.
-      </p>
     </div>
   );
 };
