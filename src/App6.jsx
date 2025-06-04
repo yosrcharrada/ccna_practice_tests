5eb94146badee44f4259484cc06aa6c7678e27c8
charradamohamed <mohamed.charrrada@gmail.com>
2025-06-04 01:19:55 +0100

    W6 Adding React props

--- src/App6.jsx
+++ src/App6.jsx
@@ -1,64 +1,41 @@
-import React, { useState } from 'react';
-
-const storiesData = [
-    { title: 'React', url: 'https://reactjs.org/', author: 'Jordan Walke', num_comments: 3, points: 4, objectID: 0 },
-    { title: 'Redux', url: 'https://redux.js.org/', author: 'Dan Abramov', num_comments: 2, points: 5, objectID: 1 },
-];
+import React from 'react';
 
 const App = () => {
-    console.log('App renders');
-
-    const [searchTerm, setSearchTerm] = useState('');
-
-    const handleSearch = (event) => {
-        setSearchTerm(event.target.value);
-    };
-
-    const filteredStories = storiesData.filter((story) =>
-        story.title.toLowerCase().includes(searchTerm.toLowerCase())
-    );
+    const stories = [
+        {
+            title: 'React',
+            url: 'https://reactjs.org/',
+            author: 'Jordan Walke',
+            num_comments: 3,
+            points: 4,
+            objectID: 0,
+        },
+        {
+            title: 'Redux',
+            url: 'https://redux.js.org/',
+            author: 'Dan Abramov',
+            num_comments: 2,
+            points: 5,
+            objectID: 1,
+        },
+    ];
 
     return (
         <div>
             <h1>My Hacker Stories</h1>
-            <Search searchTerm={searchTerm} onSearch={handleSearch} />
-            <List list={filteredStories} />
+            <List list={stories} />
         </div>
     );
 };
 
-const Search = ({ searchTerm, onSearch }) => {
-    console.log('Search renders');
-
-    return (
-        <div>
-            <label htmlFor="search">Search: </label>
-            <input id="search" type="text" value={searchTerm} onChange={onSearch} />
-            <p>Searching for <strong>{searchTerm}</strong></p>
-        </div>
-    );
-};
-
-const List = ({ list }) => {
-    console.log('List renders');
-
-    return (
-        <ul>
-            {list.map((item) => (
-                <Item key={item.objectID} item={item} />
-            ))}
-        </ul>
-    );
-};
-
-const Item = ({ item }) => {
-    console.log('Item renders');
-
-    return (
-        <li>
-            <a href={item.url}>{item.title}</a> by {item.author} ({item.num_comments} comments, {item.points} points)
-        </li>
-    );
-};
+const List = (props) => (
+    <ul>
+        {props.list.map((item) => (
+            <li key={item.objectID}>
+                <a href={item.url}>{item.title}</a>
+            </li>
+        ))}
+    </ul>
+);
 
 export default App;
