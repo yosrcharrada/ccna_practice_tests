a73dced540dc0e17425e4270518819dcdd433ad0
charradamohamed <mohamed.charrrada@gmail.com>
2025-06-04 01:48:38 +0100

    W2 My first React component

---
+++ src/App2.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const externalTitle = 'External Variable Title';
+
+const App = () => {
+    const internalTitle = 'Internal Variable Title';
+
+    return (
+        <div>
+            <h1>My First React Component</h1>
+        </div>
+    );
+};
+
+export default App;
