f14aeda690a437e5b652edcd7c6301fc00cd792d
charradamohamed <mohamed.charrrada@gmail.com>
2025-06-04 01:23:48 +0100

    W7: React controlled component

---
+++ src/App7.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+
+const stories = [
+    {
+        title: 'React',
+        url: 'https://reactjs.org/',
+        author: 'Jordan Walke',
+        num_comments: 3,
+        points: 4,
+        objectID: 0,
+    },
+    {
+        title: 'Redux',
+        url: 'https://redux.js.org/',
+        author: 'Dan Abramov',
+        num_comments: 2,
+        points: 5,
+        objectID: 1,
+    },
+];
+
+const App = () => {
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const handleSearch = (event) => {
+        setSearchTerm(event.target.value);
+    };
+
+    const filteredStories = stories.filter((story) =>
+        story.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    return (
+        <div>
+            <h1>My Hacker Stories</h1>
+            <Search searchTerm={searchTerm} onSearch={handleSearch} />
+            <List list={filteredStories} />
+        </div>
+    );
+};
+
+const Search = (props) => {
+    return (
+        <div>
+            <label htmlFor="search">Search: </label>
+            <input
+                id="search"
+                type="text"
+                value={props.searchTerm}
+                onChange={props.onSearch}
+            />
+        </div>
+    );
+};
+
+const List = (props) => (
+    <ul>
+        {props.list.map((item) => (
+            <Item key={item.objectID} item={item} />
+        ))}
+    </ul>
+);
+
+const Item = (props) => (
+    <li>
+        <a href={props.item.url}>{props.item.title}</a> by {props.item.author}
+    </li>
+);
+
+export default App;
