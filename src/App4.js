0cffcb5ac62a87783045ef1c0715885cce79d728
charradamohamed <mohamed.charrrada@gmail.com>
2025-06-04 01:55:31 +0100

    W4 React Multiple Components

---
+++ src/App4.js
@@ -0,0 +1,61 @@
+import React from 'react';
+
+const list = [
+    {
+        title: 'React',
+        url: 'https://reactjs.org/',
+        author: 'Jordan Walke',
+        num_comments: 3,
+        points: 4,
+        objectID: 0,
+    },
+    {
+        title: 'Redux',
+        url: 'https://redux.js.org/',
+        author: 'Dan Abramov',
+        num_comments: 2,
+        points: 5,
+        objectID: 1,
+    },
+];
+
+function List() {
+    return (
+        <ul>
+            {list.map(function (item) {
+                return (
+                    <li key={item.objectID}>
+                        <span>
+                            <a href={item.url}>{item.title}</a>
+                        </span>
+                        <span>{item.author}</span>
+                        <span>{item.num_comments}</span>
+                        <span>{item.points}</span>
+                    </li>
+                );
+            })}
+        </ul>
+    );
+}
+
+function Search() {
+    return (
+        <div>
+            <label htmlFor="search">Search: </label>
+            <input id="search" type="text" />
+        </div>
+    );
+}
+
+function App() {
+    return (
+        <div>
+            <h1>My Hacker Stories</h1>
+            <Search />
+            <hr />
+            <List />
+        </div>
+    );
+}
+
+export default App;
